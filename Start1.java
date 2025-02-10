6960429b2ebe66d9c17d35949b6e8bc8d807b32d
Nasir Sarkar <nasirsarkar537@gmail.com>
2025-02-11 02:06:17 +0600

    Add files via upload

---
+++ Start1.java
@@ -0,0 +1,15 @@
+import Classes.*;
+import java.lang.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Start1 
+{
+
+    public static void main(String[] args) throws Exception 
+	{
+    
+       new Welcome();
+	  
+    }
+}
